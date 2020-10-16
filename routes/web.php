8032343cf8157dffb7813dc3293bc143b1b0dc70
Alejandro <moctezuma963@alumnos.uaslp.edu.mx>
2020-10-16 11:57:43 -0500

    Definición Rutas
    Se definieron las Rutas que tendrá la aplicación, además de que se listan las modificaciones que se tendrán que hacer a HomeController

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return "home";});
+
+// Recolector
+Route::get('/muestraRecolector','HomeController@muestraRecolector');
+Route::get('/creaRecolector','HomeController@creaRecolector');
+Route::post('/creaRecolector/guardaRecolector','HomeController@guardaRecolector');
+Route::get('/eliminaRecolector/{id}','HomeController@eliminaRecolector');
+Route::get('/editaRecolector/{id}','HomeController@editaRecolector');
+Route::post('/editarRecolector/guardaEdicion','HomeController@guardaEdicionRecolector');
+
+// Recoleccion
+Route::get('/muestraRecoleccion','HomeController@muestraRecoleccion');
+Route::get('/creaRecoleccion','HomeController@creaRecoleccion');
+Route::post('/creaRecoleccion/guardaRecoleccion','HomeController@guardaRecoleccion');
+Route::get('/eliminaRecoleccion/{id}','HomeController@eliminaRecoleccion');
+Route::get('/editaRecoleccion/{id}','HomeController@editaRecoleccion');
+Route::post('/editarRecoleccion/guardaEdicion','HomeController@guardaEdicionRecoleccion');
+
+// Enlaza Recolector con Recolleccion
+Route::get('/muestraRecoleccion/{id}','HomeController@muestraEnlaces01');
+Route::get('/enlazarRecolectorRecoleccion/{id_recolector}/{id_recoleccion}','HomeController@enlace01');
