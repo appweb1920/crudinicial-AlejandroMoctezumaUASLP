72df73faa0a81f9f637dbd7d693509d3e2f92f40
Alejandro <moctezuma963@alumnos.uaslp.edu.mx>
2020-10-19 08:39:12 -0500

    Materialize y Barra de Navegacion
    Se implementa los scirpts necesarios para usar Materialize en las vistas del proyecto. Además, se rediseña la barra de navegación del sitio.

--- resources/views/VistasReciclaje/muestraRecolector.blade.php
+++ resources/views/VistasReciclaje/muestraRecolector.blade.php
@@ -4,16 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Muestra Recolector</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel = "stylesheet"  href = "https://fonts.googleapis.com/icon?family=Material+Icons">  
+    <link rel = "stylesheet"  href = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">  
+    <script type = "text/javascript"  src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>             
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>    
 </head>
 <body>
-    <h1>Muestra Recolector</h1>
+    <div class = "row" style = "width:100%;">  
+        <div class = "col s12 m12 l12">  
+            <nav>  
+                <div class="nav-wrapper light-green">  
+                    <a href="#" class="brand-logo right">Crudinicial</a>  
+                    <ul id="nav-mobile" class="left">  
+                        <li><a href="/muestraRecolector">Recolectores</a></li>  
+                        <li><a href="/muestraRecoleccion">Puntos de Reciclaje</a></li>  
+                        <li><a href="/muestraEnlaces">Detalles Recolector</a></li>  
+                    </ul>  
+                </div>  
+            </nav>  
+        </div>  
+    </div>  
 
-    <div>
-        <p><a href="/muestraRecolector">Recolectores</a></p>
-        <p><a href="/muestraRecoleccion">Puntos de Reciclaje</a></p>
-        <p><a href="/muestraEnlaces">Detalles Recolector</a></p>
-    </div>
+    <h2>Muestra Recolectores</h2>
 
     <p><a href="/creaRecolector">Nuevo Recolector</a></p>
     @if(!is_null($datos))
